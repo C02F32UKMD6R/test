12411076aa534686c01ea9cc530d1df19d207eac
soar_local_admin <root@localhost>
2023-07-31 22:29:03 +0000

    fdsa

---
+++ temp/aaa.py
@@ -0,0 +1,31 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+def on_start(container):
+    phantom.debug('on_start() called')
+
+
+
+    return
+
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
